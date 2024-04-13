04885a43c6d7725764d5d69404e352742a7e6f84
Egor A. Rusinov <erusinov@inno.tech>
2024-04-13 19:44:25 +0500

    n numbers sum in k threads

--- main.cpp
+++ main.cpp
@@ -2,12 +2,33 @@
 #include <omp.h>
 
 int main() {
-    omp_set_num_threads(4);
+    int n, k;
 
-    #pragma omp parallel
+    std::cout << "Введите k: ";
+    std::cin >> k;
+
+    std::cout << "Введите n: ";
+    std::cin >> n;
+
+    omp_set_num_threads(k);
+
+    int sum = 0;
+
+    #pragma omp parallel reduction(+: sum)
     {
-        std::cout << "Hello world!\n" << std::endl;
+        int thread_num = omp_get_thread_num();
+        int partial_sum = 0;
+
+        for (int i = thread_num + 1; i <= n; i += omp_get_num_threads()) {
+            partial_sum += i;
+        }
+
+        printf("[%d]: Sum = %d\n", thread_num, partial_sum);
+
+        sum += partial_sum;
     }
+
+    printf("Sum = %d\n", sum);
 
     return 0;
 }
