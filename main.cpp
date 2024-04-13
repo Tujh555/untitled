c825f588d08248f05600b45c0b511808508fd94b
Egor A. Rusinov <erusinov@inno.tech>
2024-04-13 19:03:36 +0500

    k threads

--- main.cpp
+++ main.cpp
@@ -3,16 +3,16 @@
 
 int main() {
     int k;
-    std::cout << "Enter the number of threads: ";
+    std::cout << "Введите k: ";
     std::cin >> k;
 
-    omp_set_num_threads(k)
+    omp_set_num_threads(k);
 
     #pragma omp parallel
     {
         int thread_num = omp_get_thread_num();
         int num_threads = omp_get_num_threads();
-        std::cout << "I am " << thread_num << " thread from " << num_threads << " threads!" << std::endl;
+        printf("I am %d thread from %d threads!\n", thread_num, num_threads);
     }
 
     return 0;
