ed59cdf68d045d160c478bba54afbd7fe2ceea94
Egor A. Rusinov <erusinov@inno.tech>
2024-04-13 19:04:49 +0500

    event threads

--- main.cpp
+++ main.cpp
@@ -12,7 +12,10 @@ int main() {
     {
         int thread_num = omp_get_thread_num();
         int num_threads = omp_get_num_threads();
-        printf("I am %d thread from %d threads!\n", thread_num, num_threads);
+
+        if (thread_num % 2 == 0) {
+            printf("I am %d thread from %d threads!\n", thread_num, num_threads);
+        }
     }
 
     return 0;
