72f025fe8a3c929b407e76cf995ef58411e1b839
Egor A. Rusinov <erusinov@inno.tech>
2024-04-29 22:34:24 +0500

    n numbers sum in k threads

--- main.cpp
+++ main.cpp
@@ -9,8 +9,6 @@ int main() {
     omp_set_num_threads(k);
     int thread_num;
 
-    // TODO спросить кого нибудь чего вообще в этой лабе хотят
-
     #pragma omp parallel shared(thread_num)
     {
         thread_num = omp_get_thread_num();
